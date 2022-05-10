6b2aadfd44ab355206d10963dadaf17919a864fd
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-05-09 22:29:15 -0400

    Added a basic navbar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <Header></Header>
-  );
+    return (
+        <><Header></Header><Navbar></Navbar></>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <navbar>
+            <a  href='/'><h2 className='nav'>About</h2></a>
+            <a  href='/'><h2 className='nav'>Portfolio</h2></a>
+            <a  href='/'><h2 className='nav'>Contact</h2></a>
+            <a  href='/'><h2 className='nav'>Resume</h2></a>
+        </navbar>
+    );
+}
+
+export default Navbar;

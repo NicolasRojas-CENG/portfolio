88ac3a9c4330e3f2f780f65a1100d34e459227de
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-05-14 16:48:51 -0400

    Added content to the contact component.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import React, { useState } from 'react';
-import Navbar from './components/Navbar';
+import Header from './components/Header'
+import Nav from './components/Navbar';
 import About from './components/About';
-import Portfolio from './components/Portfolio';
+//import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
-import './App.css'
+import './App.css';
 
 function App() {
-    const [categories] = useState([
-        {
-        name: 'About',
-        description: `${About}`},
-        { name: 'Portfolio', description: `${Portfolio}` },
-        { name: 'Contact', description: `${ContactForm}`},
-    ]);
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
 
-    const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
-    return (
-        <>
-            <header>
-                <Navbar
-                    categories={categories}
-                    setCurrentCategory={setCurrentCategory}
-                    currentCategory={currentCategory}
-                ></Navbar>
-            </header>
-            <main>
-                <Portfolio currentCategory={currentCategory}></Portfolio>
-            </main>
-        </>
-    );
+  const [contactSelected, setContactSelected] = useState(false);
+
+  return (
+    <div>
+        <Header></Header>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,32 +10,29 @@ function Navbar(props) {
     setContactSelected,
   } = props;
 
-  return (
-      <nav>
-        <ul className="flex-row">
-          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-            <span onClick={() => setContactSelected(true)}>Contact</span>
-          </li>
-          {categories.map((category) => (
-            <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && 'navActive'
-                }`}
-              key={category.name}
-            >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category);
-                  setContactSelected(false);
-                }}
-              >
-                {capitalizeFirstLetter(category.name)}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-  );
+    return (
+        <header>
+            <nav>
+                <ul className="flex-row">
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                        <span onClick={() => setContactSelected(true)}>Contact</span>
+                    </li>
+                    {categories.map((category) => (
+                        <li className={`mx-1 ${
+                            currentCategory.name === category.name && !contactSelected && 'navActive'
+                        }`} key={category.name}>
+                            <span onClick={() => {
+                                setCurrentCategory(category);
+                                setContactSelected(false);
+                            }}>
+                                {capitalizeFirstLetter(category.name)}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Navbar;

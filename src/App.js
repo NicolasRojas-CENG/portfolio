0f756e8d880036ffcd2cecc4c483b349945c9e35
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-05-19 23:22:03 -0400

    add the changes.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,39 @@
-import React, { useState } from 'react';
 import Header from './components/Header'
 import Nav from './components/Navbar';
 import About from './components/About';
-//import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
+import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [categories] = useState([
-    {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
-    },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]);
+    const [currentPage, setCurrentPage] = useState('About');
+    const renderPage = () => {
+        if (currentPage === 'About') {
+          return <About />;
+        }
+        if (currentPage === 'Contact') {
+          return <ContactForm />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+      };
 
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  const [contactSelected, setContactSelected] = useState(false);
-
-  return (
-    <>
-        <section id='section'>
-            <Header></Header>
-            <Nav
-                categories={categories}
-                setCurrentCategory={setCurrentCategory}
-                currentCategory={currentCategory}
-                contactSelected={contactSelected}
-                setContactSelected={setContactSelected}
-            ></Nav>
-        </section>
-      <main>
-        {!contactSelected ? (
-          <>
-
-            <About></About>
-          </>
-        ) : (
-          <ContactForm></ContactForm>
-        )}
-      </main>
-      <Footer></Footer>
-    </>
-  );
+      const handlePageChange = (page) => setCurrentPage(page);
+    return (
+        <>
+            <section id='section'>
+                <Header></Header>
+                <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+            </section>
+            <main>
+                {renderPage()}
+            </main>
+            <Footer></Footer>
+        </>
+    );
 }
 
 export default App;
